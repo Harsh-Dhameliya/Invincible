27f7624df23edb28a9c57af94b4d2e1a31ce1463
harsh-dhameliya <dhameliyaharsh18@gmail.com>
2024-12-20 00:35:44 +0530

    reseet

--- static/js/index.js
+++ static/js/index.js
@@ -1,18 +1,18 @@
 function change_image(src) {
   let stat = document.querySelector(".states");
   if (src == "saputara") {
-    stat.style.backgroundImage = "url('../static/images/gj-saputara.jpg')";
+    stat.style.backgroundImage = "url('../static/images/gj-vav.jpg')";
   } 
   else if (src == "goa") {
-    stat.style.backgroundImage = "url('../static/images/slide-2.jpg')";
+    stat.style.backgroundImage = "url('../static/images/goa-2.jpg')";
   } 
   else if (src == "manali") {
-    stat.style.backgroundImage = "url('../static/images/manali-2.jpg')";
+    stat.style.backgroundImage = "url('../static/images/uttrakhand-1.jpg')";
   } 
   else if (src == "rajasthan") {
     stat.style.backgroundImage = "url('../static/images/rajasthan-thar-desert.jpg')";
   } 
   else {
-    stat.style.backgroundImage = "url('../static/images/srinagar-dal-lake.jpg')";
+    stat.style.backgroundImage = "url('../static/images/jammu-1.jpg')";
   }
 }
